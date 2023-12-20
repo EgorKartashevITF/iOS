f89ef126a0bbd6767a37838996988f7613026479
Egor Kartashev <egorkarta982@gmail.com>
2023-12-20 10:50:46 +0300

    first test commit

--- LingvaVestra/ViewController.swift
+++ LingvaVestra/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     // swiftlint:disable:next unneeded_override
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Hello World")
     }
-
 }
